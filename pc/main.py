4ea64d083f97266736c66e6636dde649f4d6c239
DESKTOP-UK5Q6BA\gutte <monopeelz@gmail.com>
2017-03-28 20:23:09 +0700

    server coomit test

--- pc/main.py
+++ pc/main.py
@@ -151,10 +151,6 @@ if __name__ == '__main__':
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.bind( (HOST, 7769) )
 	sock.listen(4)
-
-
-
-
 	while True: 
 		conn, addr = sock.accept()
 
@@ -171,4 +167,4 @@ if __name__ == '__main__':
 
 	# thread controller
 
-class Server(threading.Thread):
+	#Test Commit
